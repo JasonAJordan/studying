8fb7182b78b98ea0103d5b54a393adc9e4f87a20
Jason Jordan <JasonAJordan0@gmail.com>
2021-03-20 14:53:10 -0400

    "brutish" force answer but O(n) time

--- algoexpertStuff/sortedSquaredArray.js
+++ algoexpertStuff/sortedSquaredArray.js
@@ -15,3 +15,28 @@ function sortedSquaredArray(array) {
 //o(nlogn) time | O(n) space. 
 
 //There is a faster way of doing it with o(n) time.
+
+function sortedSquaredArray2(array) {
+  // after looking at video explanation
+	
+	const sortedSquares = new Array(array.length).fill(0);
+	let smallerIdx = 0;
+	let largerIdx = array.length -1;
+	
+	for (let i = array.length -1; i >= 0; i--){
+		const smallerValue = array[smallerIdx];
+		const largerValue = array[largerIdx];
+		
+		if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+			sortedSquares[i] = smallerValue ** 2;
+			smallerIdx++;
+		} else {
+			sortedSquares[i] = largerValue ** 2;
+			largerIdx--;
+		}
+	}
+
+	return sortedSquares
+}
+
+console.log(sortedSquaredArray([1,2,3,4]));
