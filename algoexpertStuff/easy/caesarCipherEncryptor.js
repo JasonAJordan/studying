abb88a876c5f812c44eee0e0e7d42ab5ad9b5325
Jason Jordan <JasonAJordan0@gmail.com>
2021-03-24 17:55:21 -0400

    CaesarCipher

--- algoexpertStuff/easy/caesarCipherEncryptor.js
+++ algoexpertStuff/easy/caesarCipherEncryptor.js
@@ -1,3 +1,13 @@
 function caesarCipherEncryptor(string, key) {
     // Write your code here.
+      let newString = []
+      let alph = 'abcdefghijklmnopqrstuvwxyz'.split("")
+      
+      for (let i = 0; i < string.length; i ++){
+          let letter = string[i];
+          const newIdx = (alph.indexOf(letter) + key) % 26
+          const newLetter = alph[newIdx]
+          newString.push(newLetter)
+      }
+      return newString.join("");
   }
