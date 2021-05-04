9aee435e07ed6f804a889591880272ab21fe2ed4
Jason Jordan <JasonAJordan0@gmail.com>
2021-05-04 15:41:43 -0400

    1

---
+++ algoexpertStuff/etcetc/minimum inital value.js
@@ -0,0 +1,24 @@
+class Solution {
+    solve(nums) {
+
+     let output = 0;  // 0,  4
+     let sum = 0;    //2, 
+
+     for (let i = 0; i < nums.length; i++){
+         let num = nums[i]
+         sum += num
+
+         if (sum < 1){
+            let x = 1 - sum
+            output += x
+            sum += x
+         } 
+     }   
+        if (output > 0) {
+            return add
+        } else {
+            return 1
+    }
+
+
+}
