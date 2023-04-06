9f2be7ebc0750f6c5253ad6c889ea4639952bf09
unknown <jasonajordan0@gmail.com>
2023-04-06 17:34:57 -0400

    thrusday

---
+++ leetCode/easy/JS/1446 Consecutive Characters.js
@@ -0,0 +1,14 @@
+var maxPower = function(s) {
+  let power = 1
+  
+  let start = 0
+  for (let end = 1; end < s.length; end++) {
+      if (s[start] !== s[end]) {
+          start = end
+      }
+          
+      power = Math.max(power, end - start + 1)
+  }
+  
+  return power
+};
