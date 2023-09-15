c0db4ea967d53e714d1203354a4488fbf56ae5d6
unknown <jasonajordan0@gmail.com>
2023-09-15 19:26:07 -0400

    lab 9

--- hunter/135/lab 9/3d-space.cpp
+++ hunter/135/lab 9/3d-space.cpp
@@ -2,7 +2,7 @@
 Author: Jason Jordan
 Course: CSCI-135
 Instructor: Tong Yi
-Assignment: Lab 9A, B & C
+Assignment: Lab 9A, B, C & E
 
 Write a program 3d-space.cpp, 
 in which you define a function length() that receives the coordinates of a point P passed as a pointer,
@@ -36,21 +36,82 @@ Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
   return farthest;
 }
 
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+  // x' = x + vel.x * dt;
+  // y' = y + vel.x * dt; 
+  // z' = z + vel.x * dt;
+
+  ppos->x = ppos->x + (pvel->x * dt);
+  ppos->y = ppos->y + (pvel->y * dt);
+  ppos->z = ppos->z + (pvel->z * dt);
+}
+
+// allocate memory and initialize
+Coord3D* createCoord3D(double x, double y, double z){
+  // pointer to object
+  Coord3D *pos;         
+  
+  // dynamic object creation
+  pos = new Coord3D;  
+  pos->x = x;
+  pos->y = y;
+  pos->z = z;
+  
+  return pos;
+};
+
+// free memory
+void deleteCoord3D(Coord3D *p){
+  delete p;
+};
+
 int main() {
   // // for part a
   // Coord3D pointP = {10, 20, 30};
   // cout << length(&pointP) << endl; // would print 37.4166
 
-  Coord3D pointP = {10, 20, 30};
-  Coord3D pointQ = {-20, 21, -22};
 
-  cout << "Address of P: " << &pointP << endl;
-  cout << "Address of Q: " << &pointQ << endl << endl;
+  // // Part B
+  // Coord3D pointP = {10, 20, 30};
+  // Coord3D pointQ = {-20, 21, -22};
 
-  Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+  // cout << "Address of P: " << &pointP << endl;
+  // cout << "Address of Q: " << &pointQ << endl << endl;
+
+  // Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
   
-  cout << "ans = " << ans << endl; // So which point is farther?
-  // Looks like it was Q!
+  // cout << "ans = " << ans << endl; // So which point is farther?
+  // // Looks like it was Q!
 
+  // // Part C
+  // Coord3D pos = {0, 0, 100.0};
+  // Coord3D vel = {1, -5, 0.2};
+
+  // move(&pos, &vel, 2.0); // object pos gets changed
+  // cout << pos.x << " " << pos.y << " " << pos.z << endl;
+  // // prints: 2 -10 100.4
+
+  // delete &pos;
+  // delete &vel;
+
+  // Part E
+  double x, y, z;
+  cout << "Enter position: ";
+  cin >> x >> y >> z;
+  Coord3D *ppos = createCoord3D(x,y,z);
+  
+  cout << "Enter velocity: ";
+  cin >> x >> y >> z;
+  Coord3D *pvel = createCoord3D(x,y,z);
+
+  move(ppos, pvel, 10.0);
+
+  cout << "Coordinates after 10 seconds: " 
+        << (*ppos).x << " " << (*ppos).y << " " << (*ppos).z << endl;
+
+  deleteCoord3D(ppos); // release memory
+  deleteCoord3D(pvel);
+
+  
   return 0;
 }
