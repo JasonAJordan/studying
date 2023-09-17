43c99a8b2ae94f47dafbd8710696cdd92e0efade
unknown <jasonajordan0@gmail.com>
2023-09-17 11:50:28 -0400

    lab 13 done!

---
+++ hunter/135/lab 13/recursion.cpp
@@ -0,0 +1,73 @@
+/*
+Author: Jason Jordan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 13A, B, c
+
+// Part A
+Write a program recursion.cpp, defining a function
+
+void printRange(int left, int right);
+that prints all numbers in range left ≤   x   ≤ right, separated by spaces. (Don’t use loops, global or static variables.)
+
+A usage example:
+
+int main() {
+    printRange(-2, 10);
+}
+Will print:
+
+-2 -1 0 1 2 3 4 5 6 7 8 9 10
+When left > right, the range is empty and the program should not print any numbers.
+
+// Part B
+int sumRange(int left, int right);
+that computes the sum of all numbers in range left ≤   x   ≤ right. (Don’t use loops, global or static variables.)
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int sumRange(int left, int right){
+  if (left == right){
+    return right;
+  } else {
+    return left + sumRange(left + 1, right);
+  }
+
+}
+
+int sumArray(int *arr, int size){
+  if (size == 1){
+    return arr[0];
+  }
+  // cout << arr[size-1] << " " << size << endl;
+  return arr[size - 1] + sumArray(arr, size - 1);
+}
+  
+// alphanumeric check iswalnum(char)
+bool isAlphanumeric(string s){
+  if (s.length() == 0 ){
+    return true;
+  } 
+  if (s.length() == 1){
+    return iswalnum(s[0]);
+  }
+
+  // I check each char for iswalnum then make a substring and run it again until the 
+  // string hits a lens of 1
+  return iswalnum(s[0]) && isAlphanumeric(s.substr(1, s.length()));
+
+}
+
+int main() {
+
+  cout << isAlphanumeric("ABCD") << endl;        // true (1)
+  cout << isAlphanumeric("Abcd1234xyz") << endl; // true (1)
+  cout << isAlphanumeric("KLMN 8-7-6") << endl;  // false (0)
+
+  return 0;
+}
